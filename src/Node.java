239ff15398f5cb210e1ca68ebe6558137a15721b
Hans Otto Wirtz <hansottowirtz@gmail.com>
2017-10-16 11:52:58 +0200

    More code

--- src/Node.java
+++ src/Node.java
@@ -2,7 +2,11 @@ import java.net.InetAddress;
 
 public class Node {
     public InetAddress ip;
-    public Node() {
+    public String name;
+    public int id;
 
+    public Node(String name, InetAddress ip) {
+        this.name = name;
+        this.id = Util.hash(name);
     }
 }
---
+++ src/Util.java
@@ -0,0 +1,5 @@
+public class Util {
+    public static int hash(String str) {
+        return 123;
+    }
+}
