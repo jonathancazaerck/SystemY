981756799c1f87558d173cc4981bdfeba46615a1
Jonathan Cazaerck <jonathan.cazaerck@gmail.com>
2017-10-16 12:55:55 +0200

    map to json and save

--- src/NameServer.java
+++ src/NameServer.java
@@ -1,3 +1,7 @@
+import org.json.simple.JSONObject;
+
+import java.io.FileWriter;
+import java.io.IOException;
 import java.net.InetAddress;
 import java.util.HashMap;
 
@@ -16,6 +20,17 @@ public class NameServer {
         return nodeIpMap.get(Util.hash(name));
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
+    }
+    public void toJSON(HashMap<Integer, InetAddress> map){
+        JSONObject obj = new JSONObject(map);
+
+        try (FileWriter file = new FileWriter("test.txt")) {
+            file.write(obj.toJSONString());
+            System.out.println("Successfully Copied JSON Object to File...");
+            System.out.println("\nJSON Object: " + obj);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
