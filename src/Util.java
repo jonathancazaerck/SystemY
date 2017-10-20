4fc466e9387b4810ec21ceecc9b4f894a3cad14f
eliashavermans <elias.havermans@student.uantwerpen.be>
2017-10-20 11:36:58 +0200

    hashfunctie v3

--- src/Util.java
+++ src/Util.java
@@ -2,11 +2,6 @@ import static java.lang.Math.abs;
 
 public class Util {
     public static int hash(String str) {
-        int hash = 7;
-        for (int i = 0; i < str.length(); i++) {
-            hash = hash*31 + (str.charAt(i));
-        }
-        hash = Math.abs(hash);
-        return hash % 32768;
+        return Math.abs(str.hashCode()%32768);
     }
 }
