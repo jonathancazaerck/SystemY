9e92e368264b2ceaf14758f9fe8af0d4fe9630e5
Jillsealc <jclaes@hotmail.com>
2017-10-25 11:54:37 +0200

    rmi server met errors

--- src/Node.java
+++ src/Node.java
@@ -1,12 +1,15 @@
 import java.net.InetAddress;
 
+
 public class Node {
     public InetAddress ip;
     public String name;
     public int id;
+    public NodeOperations nodeOperations;
 
-    public Node(String name, InetAddress ip) {
+    public Node(String name, InetAddress ip, NodeOperations nodeOperations) {
         this.name = name;
         this.id = Util.hash(name);
+        this.nodeOperations = nodeOperations;
     }
 }
--- src/NodeMain.java
+++ src/NodeMain.java
@@ -1,6 +1,8 @@
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
 
 public class NodeMain {
     public static void main(String[] args){
@@ -23,10 +25,20 @@ public class NodeMain {
 
             //Print en schrijf JSON-file
             nameServer.toJSON(nameServer.nodeIpMap);
+
+            //RMI client
+            Registry vRegistry  = LocateRegistry.getRegistry();
+            NodeOperations vNodeOperations = (NodeOperations) vRegistry.lookup(NodeOperations.class.getName());
+            Node vConnection = new NodeMain().createNode(vNodeOperations);
+
         } catch (UnknownHostException e) {
             e.printStackTrace();
         } catch (RemoteException e) {
             e.printStackTrace();
         }
+    }
+
+    public Node createNode(NodeOperations vNodeOperations){
+        return new Node(vNodeOperations);
     }
 }
