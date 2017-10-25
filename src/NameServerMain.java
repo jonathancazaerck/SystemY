b2cd5a3f04e1c2e35bf418e7a2979005d14de0f7
Hans Otto Wirtz <hansottowirtz@gmail.com>
2017-10-25 12:23:20 +0200

    Build jars

--- src/NameServerMain.java
+++ src/NameServerMain.java
@@ -1,14 +1,14 @@
 import java.rmi.AlreadyBoundException;
-import java.rmi.Remote;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
+
 
 public class NameServerMain {
     public static void main(String[] args) {
         try{
             Registry vRegistry = LocateRegistry.getRegistry();
-            vRegistry.bind(NameServerOperations.class.getName(), (Remote) new NameServer());
+            vRegistry.bind(NameServerOperations.class.getName(), new NameServer());
             System.out.println("NameServer ready.");
         } catch (RemoteException e) {
             e.printStackTrace();
@@ -16,6 +16,4 @@ public class NameServerMain {
             e.printStackTrace();
         }
     }
-
-
 }
--- src/NodeMain.java
+++ src/NodeMain.java
@@ -1,5 +1,6 @@
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
@@ -7,38 +8,16 @@ import java.rmi.registry.Registry;
 public class NodeMain {
     public static void main(String[] args){
         try {
-            NameServer nameServer = new NameServer();
-
-            nameServer.registerNodeByName("jefke", InetAddress.getByName("127.0.0.1"));
-
-            //Proberen of er twee dezelfde nodes kunnen aangemaakt worden
-            nameServer.registerNodeByName("jefke", InetAddress.getByName("127.1.1.1"));
-
-            //Rij uit treemap printen
-            //System.out.println(nameServer.getIpByName("jefke"));
-
-            //Node deleten
-            nameServer.removeNodeByName("jefke");
-
-            //Print treemap
-            nameServer.printTreemap();
-
-            //Print en schrijf JSON-file
-            nameServer.toJSON(nameServer.nodeIpMap);
-
-            //RMI client
-            Registry vRegistry  = LocateRegistry.getRegistry();
-            NodeOperations vNodeOperations = (NodeOperations) vRegistry.lookup(NodeOperations.class.getName());
-            Node vConnection = new NodeMain().createNode(vNodeOperations);
-
+            Registry vRegistry = LocateRegistry.getRegistry();
+            NameServerOperations vNameServerOperations = (NameServerOperations) vRegistry.lookup("NameServerOperations");
+            //Node vConnection = new Node(args[0], InetAddress.getByName(args[1]), vNameServerOperations);
+            vNameServerOperations.registerNodeByName(args[0], InetAddress.getByName(args[1]));
         } catch (UnknownHostException e) {
             e.printStackTrace();
         } catch (RemoteException e) {
             e.printStackTrace();
+        } catch (NotBoundException e) {
+            e.printStackTrace();
         }
-    }
-
-    public Node createNode(NodeOperations vNodeOperations){
-        return new Node(vNodeOperations);
     }
 }
