42df328a3125f2deec28cb0f7eda7d6a9ca00c82
Jillsealc <jclaes@hotmail.com>
2017-12-18 12:09:57 +0100

    gui

---
+++ test/GUITest.java
@@ -0,0 +1,18 @@
+import ds3.SystemY_GUI;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+
+public class GUITest {
+    @Test
+    void test() {
+       SystemY_GUI gui = new SystemY_GUI();
+       gui.addToList("test1.txt");
+       gui.addToList("test2.txt");
+       gui.addToList("test3.txt");
+       gui.updateJList();
+    }
+}
