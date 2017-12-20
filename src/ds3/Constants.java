2d8937f58065760ee09123cd7c2999dca9acf71c
Jillsealc <jclaes@hotmail.com>
2017-12-20 12:18:53 +0100

    comments

--- src/ds3/Constants.java
+++ src/ds3/Constants.java
@@ -3,7 +3,7 @@ package ds3;
 public class Constants {
     public static String MULTICAST_IP = "228.5.6.9";
     public static int MULTICAST_PORT = 6789;
-    public static int DEFAULT_PORT = 6788;
+    public static int DEFAULT_PORT = 6788; //when there is no port given, the default port is used
     public static int REGISTRY_PORT = 1099;
     public static int FILE_METADATA_LENGTH = 200;
     public static int MAX_FILE_SIZE = (int) Math.pow(2, 22);
--- src/ds3/Ring.java
+++ src/ds3/Ring.java
@@ -5,10 +5,15 @@ import java.util.Map;
 import java.util.Set;
 import java.util.TreeMap;
 
+
+//This class represents the node ring. It's a subclass from Treemap. The treemap contains the hash and the ip-adres
 public class Ring extends TreeMap<Integer, InetSocketAddress> {
+
+    //Method to get the hash of the previous node
     public int lowerModularEntry(int key) {
         Map.Entry<Integer, InetSocketAddress> prevEntry = this.lowerEntry(key);
 
+        //If we search for the previous node from the first node, we will return the last node hash
         if (prevEntry == null) {
             return this.lastKey();
         } else {
@@ -16,9 +21,11 @@ public class Ring extends TreeMap<Integer, InetSocketAddress> {
         }
     }
 
+    //Method to get the hash of the next node
     public int higherModularEntry(int key) {
         Map.Entry<Integer, InetSocketAddress> nextEntry = this.higherEntry(key);
 
+        //If we search the next node from the last node, we will return the first node hash
         if (nextEntry == null) {
             return this.firstKey();
         } else {
