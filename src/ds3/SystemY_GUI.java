74f88454e41145b79ef1b69120a2652bb1a594e4
Jillsealc <jclaes@hotmail.com>
2017-12-20 11:36:20 +0100

    gui

--- src/ds3/SystemY_GUI.java
+++ src/ds3/SystemY_GUI.java
@@ -1,28 +1,34 @@
 package ds3;
 
 import javax.swing.*;
+import java.awt.*;
 import java.util.ArrayList;
+import javax.swing.JComponent;
 
 public class SystemY_GUI {
     private JList fileList;
+    private JButton button1;
     private ArrayList<String> inputList;
     DefaultListModel<String> model;
 
-    public SystemY_GUI (){
-        inputList = new ArrayList<String>();
+    public SystemY_GUI() {
+        JFrame frame = new JFrame("System Y");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
     }
 
-    public void updateJList(){
-        model = new DefaultListModel<String>();
-        for(String filename: inputList){
+    public void updateJList() {
+        /*model = new DefaultListModel<String>();
+        for (String filename : inputList) {
             model.addElement(filename);
         }
         fileList.setModel(model);
-        fileList.setSelectedIndex(0);
+        fileList.setSelectedIndex(0);*/
     }
 
-    public void addToList(String in){
-        inputList.add(in);
+    public void addToList(String in) {
+        //inputList.add(in);
     }
 
 
